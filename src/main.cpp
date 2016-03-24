50d63b165dfdff8be1c94572c4da76a304b47234
James Matta <jamesmatta@gmail.com>
2016-03-24 18:30:54 -0400

    Initial commits for a few placeholder files.
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ORCHID  -  ORnl baCkground at HfIr Daq
+A program for directly acquiring data from the array of detectors serving at the
+HFIR background monitoring wall.
+*******************************************************************************/
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
