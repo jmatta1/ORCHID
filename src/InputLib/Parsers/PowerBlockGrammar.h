58feb62bbddae20355661662cbbc0c75df580d5e
James Matta <jamesmatta@gmail.com>
2016-03-26 18:28:21 -0400

    Wrote the parser for power block data structures
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/InputLib/Parsers/PowerBlockGrammar.h
@@ -0,0 +1,87 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file PowerBlockGrammar.h
+** @author James Till Matta
+** @date 26 Mar, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details holds the grammars necessary for parsing power blocks into
+** the general par structure
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_INPUTLIB_PARSERS_POWERBLOCKGRAMMAR_H
+#define ORCHID_SRC_INPUTLIB_PARSERS_POWERBLOCKGRAMMAR_H
+
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+#include <boost/phoenix/bind/bind_member_function.hpp>
+// includes from ECHOS
+#include"InputLib/Blocks/PowerBlock.h"
+#include"utility.h"
+
+namespace InputParser
+{
+
+namespace Parsing
+{
+
+namespace qi = boost::spirit::qi;
+
+// the gammar to parse a CalculationBlock
+template <typename Iterator>
+struct PowerBlockGrammar : qi::grammar<Iterator>
+{
+	PowerBlockGrammar(PowerBlock* pb) : PowerBlockGrammar::base_type(startRule), ptr(pb)
+	{
+		namespace phoenix = boost::phoenix;
+		using qi::skip;
+		using qi::blank;
+		using qi::lexeme;
+		using qi::float_;
+		using qi::int_;
+		using qi::eol;
+		using Utility::eol_;
+        
+        //define the rules to parse the parameters
+        totalChanAvail = (lexeme["TotalChannelsAvailable"]  >> '=' > int_         [phoenix::bind(&PowerBlock::totalChannelsAvailableSet , ptr, qi::_1)] > eol_);
+        paramFile      = (lexeme["PerChannelParameterFile"] >> '=' > quotedString [phoenix::bind(&PowerBlock::perChannelParameterFileSet, ptr, qi::_1)] > eol_);
+        
+		// define the start rule which holds the whole monstrosity and set the rule to skip blanks
+		// if we skipped spaces we could not parse newlines as separators
+		startRule = skip(blank) [powerBlockRule];
+		generalBlockRule = lexeme["[PowerBlock]"] >> *eol
+                               > ( 
+                                   totalChanAvail ^ paramFile
+                                 )
+                               > lexeme["[EndBlock]"];
+	}
+
+private:
+    //base rules for the file
+    qi::rule<Iterator>                 startRule;
+	qi::rule<Iterator, qi::blank_type> powerBlockRule;
+	
+	// special sub grammars
+	Utility::QuotedString<Iterator> quotedString;
+	
+	// parameters
+	qi::rule<Iterator, qi::blank_type> totalChanAvail, paramFile;
+
+	// hold the pointer that we are going to bind to
+	PowerBlock* ptr;
+};
+
+}
+
+}
+
+#endif  // ORCHID_SRC_INPUTLIB_PARSERS_POWERBLOCKGRAMMAR_H
