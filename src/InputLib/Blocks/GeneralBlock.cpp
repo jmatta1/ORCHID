ff5c4f1926ae8f837412c69aefcbc7a34cb711f3
James Matta <jamesmatta@gmail.com>
2016-03-26 16:24:43 -0400

    Wrote the structure for GeneralBlock
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/InputLib/Blocks/GeneralBlock.cpp
@@ -0,0 +1,100 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file GeneralBlock.cpp
+** @author James Till Matta
+** @date 26 Mar, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Implementation file for the GeneralBlock data structure
+**
+********************************************************************************
+*******************************************************************************/
+
+#include"InputLib/blocks/GeneralBlock.h"
+// includes for C system headers
+// includes for C++ system headers
+#include<iostream>
+// includes from other libraries
+// includes from ECHOS
+
+namespace InputParser
+{
+
+
+GeneralBlock::GeneralBlock() : runTitle(""), warnRate(10000),
+    updateFrequency(2), runOutputDirectory(""){}
+
+// required parameters
+void GeneralBlock::runTitleSet(std::string input)
+{
+    runTitle = input;
+    runTitleSet_ = true;
+}
+
+// optional parameters
+void GeneralBlock::warnRateSet(int input)
+{
+    warnRate = input;
+    warnRateSet_ = true;
+}
+
+void GeneralBlock::updateFrequencySet(int input)
+{
+    updateFrequency = input;
+    updateFrequencySet_ = true;
+}
+
+void GeneralBlock::baseOutputDirectorySet(std::string input)
+{
+    baseOutputDirectory = input;
+    baseOutputDirectorySet_ = true;
+}
+
+bool GeneralBlock::validate()
+{
+    return (runTitleSet_ &&
+            warnRateSet_ &&
+            updateFrequencySet_ &&
+            baseOutputDirectorySet_);
+}
+
+void GeneralBlock::printValidationErrors()
+{
+    std::cout << "GeneralBlock Validation Errors:\n";
+    if(!runTitleSet_)
+    {
+        std::cout << "    RunTitle was not set\n";
+    }
+    if(!warnRateSet_)
+    {
+        std::cout << "    WarnRate was not set\n";
+    }
+    if(!updateFrequency_)
+    {
+        std::cout << "    UpdateFrequency was not set\n";
+    }
+    if(!baseOutputDirectorySet_)
+    {
+        std::cout << "    BaseOutputDirectory was not set\n";
+    }
+    std::cout << "End GeneralBlock Validation Errors\n";
+}
+
+std::ostream& operator<<(std::ostream& os, CalculationBlock const& cb) 
+{
+return os << "[GeneralBlock]\n"
+    << "    RunTitle            = \"" << cb.runTitle            << "\"\n"
+    << "    WarnRate            = "   << cb.warnRate            << "\n"
+    << "    UpdateFrequency     = "   << cb.updateFrequency     << "\n"
+    << "    BaseOutputDirectory = "   << cb.baseOutputDirectory << "\n"
+    << "[EndBlock]";
+}
+
+}
---
+++ src/InputLib/Blocks/GeneralBlock.h
@@ -0,0 +1,66 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file GeneralBlock.h
+** @author James Till Matta
+** @date 26 Mar, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Definition file for the GeneralBlock data
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_INPUTLIB_BLOCKS_GENERALBLOCK_H
+#define ORCHID_SRC_INPUTLIB_BLOCKS_GENERALBLOCK_H
+
+// includes for C system headers
+// includes for C++ system headers
+#include<string>
+// includes from other libraries
+// includes from ECHOS
+
+class ostream;
+
+namespace InputParser
+{
+
+// structure to hold the information in calculation blocks
+struct GeneralBlock
+{
+    GeneralBlock();
+    // configuration parameters
+    // required parameters
+	std::string runTitle;
+	int         warnRate; //rate in hertz to warn user at
+	int         updateFrequency; //rate in hertz to update display
+	std::string baseOutputDirectory;
+    
+    // parameter setters for binding with boost::spirit::qi
+    // required parameters
+    void runTitleSet(std::string input);
+    void warnRateSet(int input);
+	void updateFrequencySet(int input);
+	void baseOutputDirectorySet(std::string input);
+	
+	bool validate();
+	void printValidationErrors();
+	
+	// ostream operator to allow outputting of the block class
+    friend std::ostream& operator<<(std::ostream& os, CalculationBlock const& cb) ;
+
+private:
+    // parameters to make sure the required parameters were set
+    bool runTitleSet_;
+    bool warnRateSet_;
+    bool updateFrequencySet_;
+    bool runOutputDirectory_;
+};
+
+}
+#endif  // ORCHID_SRC_INPUTLIB_GENERALBLOCK_H
