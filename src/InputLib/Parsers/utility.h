deab060c7f05d5926a243a684a34ece145ca250a
James Matta <jamesmatta@gmail.com>
2016-03-26 14:43:52 -0400

    Added the first parser file, this contains the super reusable stuff
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/InputLib/Parsers/utility.h
@@ -0,0 +1,68 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file utility.h
+** @author James Till Matta
+** @date 26 Mar, 2016
+** @brief
+**
+** @copyright Copyright (C) 2015 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Header to hold utility classes and definitions for input file
+** parsing
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_INPUTLIB_PARSERS_UTILITY_H
+#define ORCHID_SRC_INPUTLIB_PARSERS_UTILITY_H
+
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+#include <boost/spirit/include/qi.hpp>
+// includes from ECHOS
+
+namespace inparser
+{
+
+namespace util
+{
+    //symbol table for reading in boolean values
+    struct BoolSymbols_ : boost::spirit::qi::symbols<char, bool>
+    {
+        BoolSymbols_(){
+            this->add("True" , true )
+                     ("true" , true )
+                     ("T"    , true )
+                     ("t"    , true )
+                     ("False", false)
+                     ("false", false)
+                     ("F"    , false)
+                     ("f"    , false);
+         }
+    } boolSymbols_;
+
+    // define my line ender here, use auto cause it is going to return a strange templated type
+    auto eol_ = boost::spirit::qi::copy((',' >> *boost::spirit::qi::eol) | +boost::spirit::qi::eol);
+
+    //grammar for parsing and returning quoted strings
+    template <typename Iterator>
+    struct QuotedString : boost::spirit::qi::grammar<Iterator, std::string()>
+    {
+        QuotedString() : QuotedString::base_type(quotedStringRule)
+        {
+            quotedStringRule = '"' > +~boost::spirit::qi::char_("\"\n") > '"';
+        }
+    private:
+        boost::spirit::qi::rule<Iterator, std::string()> quotedStringRule;
+    };
+
+}
+
+}
+
+#endif  // ORCHID_SRC_INPUTLIB_PARSERS_UTILITY_H
