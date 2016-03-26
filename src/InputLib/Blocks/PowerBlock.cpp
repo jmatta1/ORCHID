450b2e70f50c4ee5d58257ec8068c3392e4b2bdd
James Matta <jamesmatta@gmail.com>
2016-03-26 17:19:41 -0400

    Wrote the data structure to hold mpod info
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/InputLib/Blocks/PowerBlock.cpp
@@ -0,0 +1,75 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file PowerBlock.cpp
+** @author James Till Matta
+** @date 26 Mar, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Implementation file for the PowerBlock data structure
+**
+********************************************************************************
+*******************************************************************************/
+
+#include"InputLib/blocks/PowerBlock.h"
+// includes for C system headers
+// includes for C++ system headers
+#include<iostream>
+// includes from other libraries
+// includes from ORCHID
+
+namespace InputParser
+{
+
+
+PowerBlock::PowerBlock() : numberChannelsUsed(1),
+    globalCfdFraction(0.2), perChannelParameterFile(""){}
+
+// required parameters
+void PowerBlock::totalChannelsAvailableSet(int input)
+{
+    totalChannelsAvailable = input;
+    totalChannelsAvailableSet_ = true;
+}
+
+void PowerBlock::perChannelParameterFileSet(std::string input)
+{
+    perChannelParameterFile = input;
+    perChannelParameterFile_ = true;
+}
+
+bool PowerBlock::validate()
+{
+    return (totalChannelsAvailableSet_ &&
+            perChannelParameterFile_ );
+}
+
+void PowerBlock::printValidationErrors()
+{
+    std::cout << "PowerBlock Validation Errors:\n";
+    if(!totalChannelsAvailableSet_)
+    {
+        std::cout << "    TotalChannelsAvailable was not set\n";
+    }
+    if(!perChannelParameterFileSet_)
+    {
+        std::cout << "    PerChannelParameterFile was not set\n";
+    }
+    std::cout << "End PowerBlock Validation Errors\n";
+}
+
+std::ostream& operator<<(std::ostream& os, PowerBlock const& pb) 
+{
+return os << "[PowerBlock]\n"
+    << "    TotalChannelsAvailable   = \"" << pb.totalChannelsAvailable << "\"\n"
+    << "    PerChannelParameterFile  = "   << pb.perChannelOverrideFile << "\n"
+    << "[EndBlock]";
+}
+
+}
---
+++ src/InputLib/Blocks/PowerBlock.h
@@ -0,0 +1,60 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file PowerBlock.h
+** @author James Till Matta
+** @date 26 Mar, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Definition file for the PowerBlock data struct
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_INPUTLIB_BLOCKS_POWERBLOCK_H
+#define ORCHID_SRC_INPUTLIB_BLOCKS_POWERBLOCK_H
+
+// includes for C system headers
+// includes for C++ system headers
+#include<string>
+// includes from other libraries
+// includes from ORCHID
+
+class ostream;
+
+namespace InputParser
+{
+
+// structure to hold the information in calculation blocks
+struct PowerBlock
+{
+    PowerBlock();
+    // configuration parameters
+    // required parameters
+	int totalChannelsAvailable;
+	std::string perChannelParameterFile;
+    
+    // parameter setters for binding with boost::spirit::qi
+    // required parameters
+    void totalChannelsAvailableSet(int input);
+    void perChannelParameterFileSet(std::string input);
+	
+	bool validate();
+	void printValidationErrors();
+	
+	// ostream operator to allow outputting of the block class
+    friend std::ostream& operator<<(std::ostream& os, DigitizerBlock const& db) ;
+
+private:
+    // parameters to make sure the required parameters were set
+    bool totalChannelsAvailableSet_;
+    bool perChannelParameterFileSet_;
+};
+
+}
+#endif  // ORCHID_SRC_INPUTLIB_BLOCKS_POWERBLOCK_H
