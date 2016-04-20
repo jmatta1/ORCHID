4a15facf07d80208b017d7d6a82fa6d420fd343a
James Till Matta <jamesmatta@gmail.com>
2016-04-20 16:28:24 -0400

    Made the main program be a little prettier
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,34 @@ HFIR background monitoring wall.
 // includes for C system headers
 // includes for C++ system headers
 #include<iostream>
+#include<string>
 // includes from other libraries
 // includes from ORCHID
 #include"InputLib/InputLib.h"
+
+std::string orchidVersion = "0.0.1";
+
+std::string titleString =
+"==============================================================================\n"
+"==============================================================================\n"
+"==                                                                          ==\n"
+"==                                                                          ==\n"
+"==     OOOO     RR RRR          CCCCCC  HH      HH  IIIIIIIIII  DDDDD       ==\n"
+"==   OOO  OOO   RRR   RR      CCC       HH      HH  IIIIIIIIII  DD  DDD     ==\n"
+"==  OO      OO  RR      RR   CCC        HH      HH      II      DD    DDD   ==\n"
+"==  OO      OO  RR     RR   CCC         HH      HH      II      DD      DD  ==\n"
+"==  OO      OO  RR   RR     CC          HHHHHHHHHH      II      DD      DD  ==\n"
+"==  OO      OO  RRRRRR      CC          HHHHHHHHHH      II      DD      DD  ==\n"
+"==  OO      OO  RR   RR     CCC         HH      HH      II      DD      DD  ==\n"
+"==  OO      OO  RR    RR     CCC        HH      HH      II      DD    DDD   ==\n"
+"==   OOO  OOO   RR     RR     CCC       HH      HH  IIIIIIIIII  DD  DDD     ==\n"
+"==     OOOO     RR      RR      CCCCCC  HH      HH  IIIIIIIIII  DDDDD       ==\n"
+"==                                                                          ==\n"
+"==                                                                          ==\n"
+"==============================================================================\n"
+"==============================================================================\n"
+"\n";
+
 
 int main(int argc, char* argv[])
 {
@@ -19,22 +44,26 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    std::cout << titleString;
+    std::cout << "    Version: "<< orchidVersion << "\n";
     std::string inputFileName(argv[1]);
-    std::cout << "----------------------------------------------------------\n";
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
     std::cout << "Reading input from the file: " << inputFileName << "\n\n";
-
     InputParser::InputParameters params;
-
     bool success = InputParser::parseBlockInputFile(&params, inputFileName);
-
     std::cout << "Parsing: " << (success?"Succeeded":"Failed") << "\n";
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
     if (!params.validateInputParameters())
     {
-        std::cout << "Parameter Validation Failed" << "\n";
+        std::cout << "\nParameter Validation Failed" << "\n";
         params.printValidationProblems();
     }
-    std::cout << "----------------------------------------------------------\n";
-    std::cout << params << "\n";
-    std::cout << "----------------------------------------------------------\n";
+    else
+    {
+        std::cout << "\nParameter Validation Succeeded" << "\n";
+        std::cout << "------------------------------------------------------\n";
+        std::cout << params << "\n";
+        std::cout << "------------------------------------------------------\n";
+    }
     return 0;
 }
