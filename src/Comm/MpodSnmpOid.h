556825b4e03a0b24b1fddc43dc030e573277c3d5
James Till Matta <jamesmatta@gmail.com>
2016-04-29 10:40:04 -0400

    Started defining enumerations for the oid system
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Comm/MpodSnmpOid.h
+++ src/Comm/MpodSnmpOid.h
@@ -12,8 +12,34 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 **
-** @details Holds the function prototypes for the functions called by
-** int main(...) to actually parse and validate the various data files
+** @details Holds the template system that builds the command strings
 **
 ********************************************************************************
 *******************************************************************************/
+#ifndef ORCHID_SRC_COMM_MPODSNMPOID_H
+#define ORCHID_SRC_COMM_MPODSNMPOID_H
+
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+#include"Utility/TmpStrConcat.h"
+
+enum class CrateSubTrees : char
+{        System = 1,       Input = 2,  Output = 3, Sensor = 4,
+  Communication = 5, PowerSupply = 6, FanTray = 7,   Rack = 8,
+         Signal = 9};
+
+enum class SystemVals : short
+{     SysMainSwitch =    1,                     SysStatus =    2,   SysVmeSysReset =    3, SysHardwareReset =    4,
+ SysFactoryDefaults =    5, SysConfigDoMeasurementCurrent =   10, SysOperatingTime =   11,  SysDebugMemory8 = 1024,
+   SysDebugMemory16 = 1025,              SysDebugMemory32 = 1026,         SysDebug = 1027,  SysDebugDisplay = 1028,
+       SysDebugBoot = 1029};
+
+enum class OutputVals : 
+{   OutputNumber = 1, OutputTable = 2, GroupsNumber = 3, GroupsTable = 4,
+    ModuleNumber = 5, ModuleTable = 6}; 
+
+static const char* WienerCrateOid = ".1.3.6.1.4.1.19947.1";
+
+#endif //ORCHID_SRC_COMM_MPODSNMPOID_H
