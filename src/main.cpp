5a22f7510f52300021f27368b0d2c462fbe5c60d
James Till Matta <jamesmatta@gmail.com>
2016-04-29 15:47:20 -0400

    Big refactor, dropping boost asio for mpod
    Dropping the boost asio based connection to the mpod crate for now. It looks
    like it will be easier to simply use popen from the C libraries and to gain
    access to the data that snmp get, set, and walk generate.
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,6 @@ HFIR background monitoring wall.
 #include<string>
 // includes from other libraries
 // includes from ORCHID
-#include"Utility/TitleString.h"
-#include"Utility/ParseAndValidate.h"
-#include"InputLib/InputLib.h"
-#include"HVLib/MpodSnmpUtil.h"
-#include"Comm/SnmpConnection.h"
 
 void snmpPrint(std::string result, int num)
 {
@@ -58,19 +53,5 @@ int main(int argc, char* argv[])
     std::cout << mpodChannelData << "\n";
     std::cout << "----------------------------------------------------------\n";
     
-    std::cout << "\n\nTesting template metaprogramming: \n"
-              << SnmpReadCmd<CrateSubTrees::System, SystemVals, SystemVals::SysMainSwitch>::cmd()
-              << "\n\n" << std::endl;
-    
-    boost::asio::io_service io_service;
-    SnmpConnection snmpConn(io_service);
-    std::cout << "Build snmp con" << std::endl;
-    snmpConn.connect(SNMP_VERSION_2c,string("192.168.11.11"), string("guru"));
-    std::cout << "connected" << std::endl;
-    snmpConn.asyncSnmpGet(string(".1.3.6.1.4.1.19947.1.1.1.0"), boost::bind(snmpPrint,_1,3));
-    std::cout << "running call" << std::endl;
-    io_service.run();
-    std::cout << "done" << std::endl;
-    return 0;
 }
 
