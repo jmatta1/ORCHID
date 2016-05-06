eb0f16f0f1b86dd5da2a54944ac221f17070e20d
James Till Matta <jamesmatta@gmail.com>
2016-05-06 10:07:44 -0400

    Tweaked the output format
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/InputLib/Blocks/MpodChannelData.cpp
+++ src/InputLib/Blocks/MpodChannelData.cpp
@@ -155,17 +155,17 @@ std::ostream& operator<<(std::ostream& os, MpodChannelData const& mcd)
     using std::setfill;
     using std::setprecision;
     using std::fixed;
-    os << "Board #, Channel #, Active, Rise Rate(V/s), Fall Rate(V/s), Voltage(V), Max I(uA), Max I Trip Time(ms)\n";
+    os << "Board, Chan, On, Rise Rate(V/s), Fall Rate(V/s), Voltage(V), Max I(uA), Trip Time(ms)\n";
     for(int i=0; i<mcd.channel.size(); ++i)
     {
-        os << setw(7)  << setfill(' ')                             << mcd.board[i]              << ", ";
-        os << setw(9)  << setfill(' ')                             << mcd.channel[i]            << ", ";
-        os << setw(6)  << setfill(' ')                             << (mcd.online[i] ? "T":"F") << ", ";
+        os << setw(5)  << setfill(' ')                             << mcd.board[i]              << ", ";
+        os << setw(4)  << setfill(' ')                             << mcd.channel[i]            << ", ";
+        os << setw(2)  << setfill(' ')                             << (mcd.online[i] ? "T":"F") << ", ";
         os << setw(14) << setfill(' ')                             << mcd.rampUpRate[i]         << ", ";
         os << setw(14) << setfill(' ')                             << mcd.rampDownRate[i]       << ", ";
         os << setw(10) << setfill(' ') << fixed << setprecision(1) << mcd.voltage[i]            << ", ";
         os << setw(9)  << setfill(' ') << fixed << setprecision(1) << mcd.maxCurrent[i]         << ", ";
-        os << setw(19) << setfill(' ')                             << mcd.currentTripTime[i]    << "\n";
+        os << setw(13) << setfill(' ')                             << mcd.currentTripTime[i]    << "\n";
     }
     return os;
 }
