cba5907fc28277132729daaa25b1e8bfaf44d88e
James Till Matta <jamesmatta@gmail.com>
2016-05-23 14:15:52 -0400

    Started implementing the user interface thread'
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/Threads/UIThread.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file UIThread.h
+** @author James Till Matta
+** @date 27 Apr, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition of the class that is run by main to handle the
+** user interface
+**
+********************************************************************************
+*******************************************************************************/
+
+#include"UIThread.h"
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
---
+++ src/Threads/UIThread.h
@@ -0,0 +1,38 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file UIThread.h
+** @author James Till Matta
+** @date 27 Apr, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition of the class that is run by main to handle the
+** user interface
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_THREADS_UITHREAD_H
+#define ORCHID_SRC_THREADS_UITHREAD_H
+
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+
+namespace Threads
+{
+class UIThread
+{
+public:
+    UIThread(){}
+    ~UIThread(){}
+};
+
+}
+#endif //ORCHID_SRC_THREADS_UITHREAD_H
