d38b6ec543d9f1a24643f11cbf65695bcbe908e6
James Till Matta <jamesmatta@gmail.com>
2016-05-24 15:26:13 -0400

    Separated functions and work in UI thread
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Threads/UIThread.cpp
+++ src/Threads/UIThread.cpp
@@ -25,16 +25,23 @@
 #include<sstream>
 // includes from other libraries
 #include<boost/thread.hpp>
+#include<boost/algorithm/string.hpp>
 #include<ncurses.h>
 // includes from ORCHID
+#include"UICommandTable.h"
+
 namespace Threads
 {
-UIThread::UIThread(SlowControls::SlowData* slDat, int refreshFrequency):
-    slowData(slDat)
+UIThread::UIThread(SlowControls::SlowData* slDat,
+                   FastData::RateData* rtDat,
+                   int refreshFrequency):
+    slowData(slDat), rateData(rtDat), persistCount(0), command(""),
+    persistentMessage(""), runLoop(true), refreshRate(refreshFrequency)
 {
     //calculate the refresh period in seconds then multiply by one billion to get
     //nanoseconds, which is what boost thread takes
     float refPeriod = 1000000000.0*(1.0/static_cast<float>(refreshFrequency));
+    this->rateMultiplier = static_cast<float>(refreshFrequency);
     this->refreshPeriod = boost::chrono::nanoseconds(static_cast<long long int>(refPeriod));
 }
 
@@ -46,46 +53,17 @@ void UIThread::operator() ()
     keypad(stdscr, true);
     cbreak();
     nodelay(stdscr, true);
-    //make the stuff used to hanndle characters typed as we go
+    //holds characters that were input
     int inChar;
-    std::string command;
-    printw("ncurses has been initiallized");
-    boost::this_thread::sleep_for(this->refreshPeriod);
-    bool runFlag = true;
-    while(runFlag)
+    printw("ncurses has been initialized");
+    boost::this_thread::sleep_for(boost::chrono::nanoseconds(500000000));
+    clear();
+    while(runLoop)
     {
         //here we check if a key was pressed
-        if((inChar=getch())!=ERR)
+        if((inChar = getch()) != ERR)
         {
-            int lastCharInd = (command.size() - 1);
-            switch(inChar)
-            {
-            case KEY_ENTER:
-            case '\r':
-            case '\n':
-            case '\f':
-                //TODO better command execution
-                if(command=="quit" || command=="exit")
-                {
-                    runFlag = false;
-                }
-                command.clear();
-                clear();
-                break;
-            case KEY_BACKSPACE:
-            case KEY_DC:
-                wmove(stdscr, 20, lastCharInd);
-                printw(" ");
-                command.erase(lastCharInd, 1);
-                break;
-            case KEY_CTAB:
-                // translate tabs into spaces
-                command.append(1, ' ');
-                break;
-            default:
-                command.append(1, inChar);
-                break;
-            }
+            this->handleKeyPress(inChar);
         }
         //here we draw the screen
         //draw the command in progress
@@ -95,6 +73,129 @@ void UIThread::operator() ()
     }
 
     endwin();
+}
+
+void UIThread::handleCommand()
+{
+    //normalize the command to lowercase
+    boost::to_lower(command);
+    //look up the command inside dispatch map
+    std::map::iterator cmdFind = UI_COMMAND_DISPATCH.find(command);
+    //clear the command and screen
+    command.clear();
+    clear();
+    // get the command enumeration or set it to invalid
+    UICommand cmd;
+    if(cmdFind != UI_COMMAND_DISPATCH.end())
+    {
+        cmd = cmdFind->second;
+    }
+    else
+    {
+        cmd = UICommands::Invalid;
+    }
+    
+    // switch on the command enum to figure out what to do
+    switch()
+    {
+    case UICommands::Invalid:
+        this->persistentMessage = "Invalid Command";
+        this->persistCount = refreshRate*5;
+        break;
+    case UICommands::Quit:
+        this->runLoop = false;
+        break;
+    default:
+        this->persistentMessage = "Got to command loop default. This should be IMPOSSIBLE!";
+        this->persistCount = refreshRate*10;
+        break;
+    }
+}
+
+void UIThread::handleKeyPress(int inChar)
+{
+    int lastCharInd = (command.size() - 1);
+    switch(inChar)
+    {
+    case KEY_ENTER:
+    case '\r':
+    case '\n':
+    case '\f':
+        this->handleCommand();
+        break;
+    case KEY_BACKSPACE:
+    case KEY_DC:
+        wmove(stdscr, 20, lastCharInd);
+        printw(" ");
+        command.erase(lastCharInd, 1);
+        break;
+    case 'a'://I know it is probably a bit silly, but this was my first thought
+    case 'b'://on how to make certain that only non silly characters make it
+    case 'c'://to the command
+    case 'd':
+    case 'e':
+    case 'f':
+    case 'g':
+    case 'h':
+    case 'i':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+    case 'n':
+    case 'o':
+    case 'p':
+    case 'q':
+    case 'r':
+    case 's':
+    case 't':
+    case 'u':
+    case 'v':
+    case 'w':
+    case 'x':
+    case 'y':
+    case 'z':
+    case 'A':
+    case 'B':
+    case 'C':
+    case 'D':
+    case 'E':
+    case 'F':
+    case 'G':
+    case 'H':
+    case 'I':
+    case 'J':
+    case 'K':
+    case 'L':
+    case 'M':
+    case 'N':
+    case 'O':
+    case 'P':
+    case 'Q':
+    case 'R':
+    case 'S':
+    case 'T':
+    case 'U':
+    case 'V':
+    case 'W':
+    case 'X':
+    case 'Y':
+    case 'Z':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        command.append(1, inChar);
+        break;
+    default://anything not listed explicitly, ignore
+        break;
+    }
 }
 
 }
--- src/Threads/UIThread.h
+++ src/Threads/UIThread.h
@@ -22,25 +22,49 @@
 
 // includes for C system headers
 // includes for C++ system headers
+#include<string>
 // includes from other libraries
 #include<boost/chrono.hpp>
 // includes from ORCHID
 #include"SlowControls/SlowData.h"
+#include"DigiLib/RateData.h"
 namespace Threads
 {
 class UIThread
 {
 public:
-    UIThread(SlowControls::SlowData* slDat, int refreshFrequency);
+    UIThread(SlowControls::SlowData* slDat, FastData::RateData* rtDat, int refreshFrequency);
     ~UIThread(){}
 
     //this is the function that is called by boost::thread when making a thread
     void operator() ();
 private:
-    //we may have a pointer to the slowData but we do not own it so no deletions
+    //Private Member Functions
+    //This function is used to handle key presses
+    void handleKeyPress(int inChar);
+    //This function handles commands when the user presses enter
+    void handleCommand();
+    
+    //Private Member Variables
+    
+    //we may have a pointer to these data but we do not own it so: No deletions!
     // *vader says shaking his finger at Boba Fett*
     SlowControls::SlowData* slowData;
+    FastData::RateData* rateData;
+    //amount of time to sleep for
     boost::chrono::nanoseconds refreshPeriod;
+    //multiplier for calculating rate from number of triggers
+    float rateMultiplier;
+    //the actual refresh rate in Hz
+    int refreshRate;
+    //string to hold commands in
+    std::string command;
+    //string to hold persistent messages in
+    std::string persistentMessage;
+    //counter for number of refreshes persistent messages persist for
+    long persistCount;
+    //flag to continue or kill loop
+    bool runLoop;
 };
 
 }
