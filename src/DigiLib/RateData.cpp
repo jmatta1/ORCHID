d40907aafd9d5d74de5b6eeb11fa11d258577b53
James Till Matta <jamesmatta@gmail.com>
2016-05-24 13:53:20 -0400

    Skeleton file for trigger rate info
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/DigiLib/RateData.cpp
@@ -0,0 +1,31 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file RateData.cpp
+** @author James Till Matta
+** @date 27 Apr, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the implementation of the class that is used to accumulate
+** trigger information from the digitizers, that in turn is used to calculate
+** trigger rates by the ui
+**
+********************************************************************************
+*******************************************************************************/
+#include"RateData.h"
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+namespace Digitizer
+{
+
+
+}
+
---
+++ src/DigiLib/RateData.h
@@ -0,0 +1,36 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file RateData.h
+** @author James Till Matta
+** @date 27 Apr, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition of the class that is used to accumulate trigger
+** information from the digitizers, that in turn is used to calculate trigger
+** rates by the ui
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_DIGILIB_RATEDATA_H
+#define ORCHID_SRC_DIGILIB_RATEDATA_H
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+namespace Digitizer
+{
+
+struct RateData
+{
+
+};
+
+}
+#endif //ORCHID_SRC_DIGILIB_RATEDATA_H
