5662f3ab9772e7ea4f1f8503a4742948d4e7948c
James Till Matta <jamesmatta@gmail.com>
2016-05-25 10:32:25 -0400

    Added maps for cmd validity in diff modes
    Different commands are accepted in different modes. I added maps that let
    the system determine if for a given mode a command is acceptable.
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Threads/UICommandTable.h
+++ src/Threads/UICommandTable.h
@@ -31,13 +31,61 @@ namespace Threads
 {
 
 //The various enumerated commands
-enum class UICommands : char {Quit, Invalid};
+enum class UICommands : char {TurnOn, TurnOff, Start, Stop,
+                              Name,   Number,  Next,  Quit,
+                              Invalid, Unavailable};
 
 //A lookup between string and the command
 static const std::map<std::string, UICommands> UI_COMMAND_DISPATCH =
     map_list_of("exit", UICommands::Quit)
-        ("quit", UICommands::Quit)
-        ("nope", UICommands::Quit);
+        ("quit",    UICommands::Quit)
+        ("nope",    UICommands::Quit)
+        ("turnon",  UICommands::TurnOn)
+        ("turnoff", UICommands::TurnOff)
+        ("start",   UICommands::Start)
+        ("stop",    UICommands::Stop)
+        ("name",    UICommands::Name)
+        ("number",  UICommands::Number)
+        ("next",    UICommands::Next);
+
+//A lookup for valid commands while in init mode
+static const std::map<UICommands, bool> INIT_MODE_VALID_CMD =
+    map_list_of(UICommands::Quit, true)
+        (UICommands::Quit,      true)
+        (UICommands::Quit,      true)
+        (UICommands::TurnOn,    true)
+        (UICommands::TurnOff,   false)
+        (UICommands::Start,     false)
+        (UICommands::Stop,      false)
+        (UICommands::Name,      false)
+        (UICommands::Number,    false)
+        (UICommands::Next,      false);
+
+//A lookup for valid commands while in idle mode
+static const std::map<UICommands, bool> IDLE_MODE_VALID_CMD =
+    map_list_of(UICommands::Quit, true)
+        (UICommands::Quit,      true)
+        (UICommands::Quit,      true)
+        (UICommands::TurnOn,    false)
+        (UICommands::TurnOff,   true)
+        (UICommands::Start,     true)
+        (UICommands::Stop,      false)
+        (UICommands::Name,      true)
+        (UICommands::Number,    true)
+        (UICommands::Next,      false);
+
+//A lookup for valid commands while in run mode
+static const std::map<UICommands, bool> RUN_MODE_VALID_CMD =
+    map_list_of(UICommands::Quit, true)
+        (UICommands::Quit,      true)
+        (UICommands::Quit,      true)
+        (UICommands::TurnOn,    false)
+        (UICommands::TurnOff,   false)
+        (UICommands::Start,     false)
+        (UICommands::Stop,      true)
+        (UICommands::Name,      false)
+        (UICommands::Number,    false)
+        (UICommands::Next,      true);
 }
 
 #endif //ORCHID_SRC_THREADS_UICOMMANDTABLE_H
