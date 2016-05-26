28eb2a972ef44d55d7af1227d86faa1c49aa0633
James Till Matta <jamesmatta@gmail.com>
2016-05-26 13:23:39 -0400

    Added leading comments and pseudocode
    As the structure of the program gels I am adding information in the form of
    comments and pseudocode in the main function to sort of show the intended
    order / form of things
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -66,40 +66,147 @@ int main(int argc, char* argv[])
     std::cout << "--------------------------------------------------------------------------------\n";
     //TODO: output the data from the digitizer csvs and the mapping csv
     std::cout << "\nReady to start!" << std::endl;
-    std::cout << "Press enter to continue" <<std::endl;
-    std::cout << "\n" << std::endl;
+    std::cout << "Press enter to continue\n" <<std::endl;
     std::cin.get();
 
-    //build interthread queues and data strutures
+    /*
+     * Build the InterThread Statistics structures
+     */
     //count the number of voltage channels
+    std::cout << "Building statistics accumulators" << std::endl;
     int numVoltageChannels = std::count(mpodChannelData.online.begin(),
                                         mpodChannelData.online.end(), true);
-    int numTemperatureChannels = 0;
-    InterThread::SlowData* slowData = new SlowControls::SlowData(numVoltageChannels,
-                                                                   numTemperatureChannels);
-    int numDigitizerChannels = 0;
-    InterThread::RateData* rateData = new FastData::RateData(numDigitizerChannels);
+    //count the number of temperature channels
+    int numTemperatureChannels = 4;
+    //Make the slow controls data structure
+    InterThread::SlowData* slowData = new InterThread::SlowData(numVoltageChannels,
+                                                                 numTemperatureChannels);
+    //count the number of digitizer channels
+    int numDigitizerChannels = 4;
+    //Make the rate data structure
+    InterThread::RateData* rateData = new InterThread::RateData(numDigitizerChannels);
+    //Make the data structure for file information
+    InterThread::FileData* fileData = new InterThread::FileData();
+    fileData->setRunTitle(params.generalBlock->runTitle);
+    
+    /*
+     * Build the InterThread Control structures
+     */
+    std::cout << "Building thread control data structures" << std::endl;
+    // For controlling DigitizerThread
+    
+    // For controlling SlowControlsThread
+    
+    // For controlling FileOutputThread
+    
+    // For controlling EventProcThread(s)
+    
+    /*
+     * Build the Device Control structures
+     */
+    std::cout << "Building device control data structures" << std::endl;
+    // For Controlling the MPOD (The temperature system needs no control)
+    
+    
+    // For Controlling the digitizer
+    
+    /*
+     * Build the InterThread Buffer/Data queues
+     */
+    std::cout << "Building interthread data queues" << std::endl;
+    // For transferring full data buffers from the digitizer
+    // reader to the event processing threads
+    
+    // For transferring empty data buffers from the event
+    // processing threads to the digitizer reader
+    
+    // For transferring full event buffers from the event
+    // processing threads to the file thread
+    
+    // For transferring empty event buffers from the file
+    // thread to the event processing threads
+    
+    // For transferring full slow controls buffers from the
+    // slow control threads to the file thread
+    
+    // For transferring empty slow controls buffers from the
+    // file thread to the slow control thread
+    
+    /*
+     * Build the callable objects for boost::thread
+     */
+    std::cout << "Building callable objects" << std::endl;
+    //make the UI thread callable
+    Threads::UIThread* uiThreadCallable =
+            new Threads::UIThread(slowData, rateData, fileData,
+                      //Future:   digitizerThreadControl, slowControlsThreadControl,
+                      //Future:   fileThreadControl, eventProcThreadControl,
+                      //Future:   digitizerControl, mpodControl
+                                  params.generalBlock->updateFrequency);
+    //make the file thread callable
+    //Threads::FileThread* fileThreadCallable = new FileThread(...);
+    
+    //make the slow controls callable
+    //Threads::SlowControlsThread slowControlsThreadCallable =
+    //          new SlowControlsThread(...);
+    
+    //make the digitizer reader callable
+    //Threads::DigitizerThread* digitizerThreadCallable =
+    //          new DigitizerThread(...);
+    
+    //make the event processing callables
+    //Threads::EventProcessingThread** evProcThreadCallable =
+    //          new EventProcessingThread*[numProcThreads];
+    //for(int i=0; i < numProcThreads; ++i)
+    //{
+    //  evProcThreadCallable[i] = new EventProcessingThread(i, ...);
+    //}
+    
 
-    //create the various thread callable objects
-    Threads::UIThread* ui = new Threads::UIThread(slowData,
-                                                  rateData,
-                                                  params.generalBlock->updateFrequency);
+    /*
+     * Handle the threads
+     */
+    std::cout << "Building threads\n" << std::endl;
+    // Make the threads
+    boost::thread uiThread(*uiThreadCallable);
+    //boost::thread fileThread(fileThreadCallable);
+    //boost::thread slowControlsThread(slowControlsThreadCallable);
+    //boost::thread digitizerThread(digitizerThreadCallable);
+    //boost::thread_group eventProcessingThreads;
+    /*for(int i = 0; i < numProcThreads; ++i)
+    {
+        eventProcessingThreads.create_thread(*(evProcThreadCallable[i]));
+    }*/
+    
+    
+    // Detach all threads except the IO thread
+    
 
-    //start the threads except the IO thread and detach them
-
-    //start the IO thread and join it
-    boost::thread uiThread(*ui);
+    //Wait to join the IO thread
     uiThread.join();
 
     //the IO thread has joined proceed with shutdown
-    std::cout << "IO thread has rejoined main the thread.\nDeleting thread callable objects" << std::endl;
+    std::cout << "UI thread has rejoined main the thread." << std::endl;
+    std::cout << "Other threads should have terminated.\n" << std::endl;
+    std::cout << "Deleting thread callable objects" << std::endl;
     //delete the thread callable objects
-    delete ui;
+    delete uiThreadCallable;
 
+    //delete the data queues
+    std::cout << "Deleting interthread data queues" << std::endl;
+    
 
-    std::cout << "Deleting the interprocess communication shared objects" << std::endl;
+    //delete the device control structures
+    std::cout << "Deleting device controls" << std::endl;
+    
+    //delete the thread control structures
+    std::cout << "Deleting thread controls" << std::endl;
+    
+    std::cout << "Deleting statistics accumulators\n" << std::endl;
     //delete shared objects generated for interprocess communication
     delete slowData;
+    delete rateData;
+    delete fileData;
 
 
     std::cout << "Closing ORCHID, have a nice day! :-)\n\n" << std::endl;
