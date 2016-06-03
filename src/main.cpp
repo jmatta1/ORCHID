32102db4f641f0b7ca6e515ba9f2fd9540eb0d23
James Till Matta <jamesmatta@gmail.com>
2016-06-03 16:59:51 -0400

    Added logging capabilities with boost::log
    Was having some trouble with incorrect interpretation of the values returned
    from snmp commands that gave status in "bits" format. To track this I needed
    output, but the output needed to be thread safe and not interfere with the
    ncurses stuff. To accomplish this I looked into the boost::log facility.
    The changes in this log represent what is needed to make a log that is
    globally accessible to all threads. All one needs to do is include
    "Utility/OrchidLogger.h" and go from there.
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,17 @@ HFIR background monitoring wall.
 #include<algorithm>
 // includes from other libraries
 #include<boost/thread.hpp>
+#define BOOST_LOG_DYN_LINK 1
+#include<boost/log/utility/setup.hpp>
+#include<boost/log/utility/setup/file.hpp>
+#include<boost/log/sinks/text_file_backend.hpp>
+#include<boost/log/utility/setup/common_attributes.hpp>
+#include <boost/core/null_deleter.hpp>
 // includes from ORCHID
 // ORCHID utilities
 #include"Utility/TitleString.h"
 #include"Utility/ParseAndValidate.h"
+#include"Utility/OrchidLogger.h"
 // ORCHID interprocess communication data objects
 #include"InterThreadComm/Data/SlowData.h"
 #include"InterThreadComm/Data/RateData.h"
@@ -40,7 +47,24 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    std::cout << Resources::titleString;
+    //initialize the logging file
+    boost::log::add_file_log(
+                boost::log::keywords::file_name = "orchid_%N.log",          //file name format
+                boost::log::keywords::rotation_size = (1*1024*1024),        //rotate to a new file every megabyte
+                boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),  //or rotate at midnight
+                boost::log::keywords::format = "[%TimeStamp%]: %Message%");  //give every message a timestamp
+    //set up to dump to the console as well
+    //boost::log::add_console_log();
+    boost::log::add_common_attributes();
+    
+    boost::shared_ptr<std::ostream> coutStream(&std::cout, boost::null_deleter());
+    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> text_sink;
+    boost::shared_ptr<text_sink> coutSink = boost::make_shared<text_sink>();
+    coutSink->locked_backend()->add_stream(coutStream);
+    boost::log::core::get()->add_sink(coutSink);
+    
+    boost::log::sources::severity_logger_mt<LogSeverity>& lg = OrchidLog::get();
+    BOOST_LOG_SEV(lg, Information) << Resources::titleString;
 
     //parse the input file and ancillary csv files
     InputParser::InputParameters params;
@@ -57,30 +81,31 @@ int main(int argc, char* argv[])
 
     mpodModuleData.sort();
     mpodChannelData.sort();
-    std::cout << std::flush << "\n\nInput Parameters\n";
-    std::cout << "================================================================================\n\n";
-    std::cout << "--------------------------------------------------------------------------------\n";
-    std::cout << "Base Parameter Input File\n";
-    std::cout << params << "\n";
-    std::cout << "--------------------------------------------------------------------------------\n";
-    std::cout << "--------------------------------------------------------------------------------\n";
-    std::cout << "MPOD Module Input File\n";
-    std::cout << mpodModuleData << "\n";
-    std::cout << "--------------------------------------------------------------------------------\n";
-    std::cout << "--------------------------------------------------------------------------------\n";
-    std::cout << "MPOD Channel Input File\n";
-    std::cout << mpodChannelData << "\n";
-    std::cout << "--------------------------------------------------------------------------------\n";
+    BOOST_LOG_SEV(lg, Information)  << "\n\nInput Parameters";
+    BOOST_LOG_SEV(lg, Information)  << "================================================================================\n";
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "Base Parameter Input File";
+    BOOST_LOG_SEV(lg, Information)  << params;
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "MPOD Module Input File";
+    BOOST_LOG_SEV(lg, Information)  << mpodModuleData ;
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "MPOD Channel Input File";
+    BOOST_LOG_SEV(lg, Information)  << mpodChannelData ;
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
     //TODO: output the data from the digitizer csvs and the mapping csv
-    std::cout << "\nReady to start!" << std::endl;
-    std::cout << "Press enter to continue\n" <<std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "\nReady to start!" << std::flush;
+    BOOST_LOG_SEV(lg, Information)  << "Press enter to continue\n" <<std::flush;
     std::cin.get();
+    BOOST_LOG_SEV(lg, Information)  << "Proceeding" << std::flush;
 
     /*
      * Build the InterThread Statistics structures
      */
     //count the number of voltage channels
-    std::cout << "Building statistics accumulators" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Building statistics accumulators" << std::flush;
     int numVoltageChannels = std::count(mpodChannelData.online.begin(),
                                         mpodChannelData.online.end(), true);
     //count the number of temperature channels
@@ -99,7 +124,7 @@ int main(int argc, char* argv[])
     /*
      * Build the InterThread Control structures
      */
-    std::cout << "Building thread control data structures" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Building thread control data structures" << std::flush;
     // For controlling DigitizerThread
     
     // For controlling SlowControlsThread
@@ -112,7 +137,7 @@ int main(int argc, char* argv[])
     /*
      * Build the Device Control structures
      */
-    std::cout << "Building device control data structures" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Building device control data structures" << std::flush;
     // For Controlling the MPOD (The temperature system needs no control)
     SlowControls::SnmpUtilControl* mpodSnmpController = new SlowControls::SnmpUtilControl(params.powerBlock->mpodIpAddress,
                                                                                           params.powerBlock->weinerMibFileDirectory);
@@ -124,7 +149,7 @@ int main(int argc, char* argv[])
     /*
      * Build the InterThread Buffer/Data queues
      */
-    std::cout << "Building interthread data queues" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Building interthread data queues" << std::flush;
     // For transferring full data buffers from the digitizer
     // reader to the event processing threads
     
@@ -146,7 +171,7 @@ int main(int argc, char* argv[])
     /*
      * Build the callable objects for boost::thread
      */
-    std::cout << "Building callable objects" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Building callable objects" << std::flush;
     //make the UI thread callable
     Threads::UIThread* uiThreadCallable =
             new Threads::UIThread(slowData, rateData, fileData,
@@ -182,9 +207,11 @@ int main(int argc, char* argv[])
     /*
      * Handle the threads
      */
-    std::cout << "Building threads\n" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Building threads\n" << std::flush;
     // Make the threads
     boost::thread scThread(*scThreadCallable);
+    BOOST_LOG_SEV(lg, Information)  << "Starting UI Thread, stopping console logging\n" << std::flush;
+    boost::log::core::get()->remove_sink(coutSink);
     boost::thread uiThread(*uiThreadCallable);
     //boost::thread fileThread(fileThreadCallable);
     //boost::thread slowControlsThread(slowControlsThreadCallable);
@@ -198,37 +225,37 @@ int main(int argc, char* argv[])
 
     //Wait to join the IO thread
     uiThread.join();
-
+    boost::log::core::get()->add_sink(coutSink);
     //the IO thread has joined proceed with shutdown
-    std::cout << "UI thread has rejoined main the thread." << std::endl;
-    std::cout << "Other threads should have terminated.\n" << std::endl;
-    std::cout << "Deleting thread callable objects" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "UI thread has rejoined main the thread. Resuming console logging." << std::flush;
+    BOOST_LOG_SEV(lg, Information)  << "Other threads should have terminated.\n" << std::flush;
+    BOOST_LOG_SEV(lg, Information)  << "Deleting thread callable objects" << std::flush;
     //delete the thread callable objects
     delete uiThreadCallable;
     delete scThreadCallable;
 
     //delete the data queues
-    std::cout << "Deleting interthread data queues" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Deleting interthread data queues" << std::flush;
     
 
     //delete the device control structures
-    std::cout << "Deleting device controls" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Deleting device controls" << std::flush;
     delete mpodController;
     delete mpodReader;
     delete mpodSnmpController;
     
     //delete the thread control structures
-    std::cout << "Deleting thread controls" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Deleting thread controls" << std::flush;
     delete sctController;
     
-    std::cout << "Deleting statistics accumulators\n" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "Deleting statistics accumulators\n" << std::flush;
     //delete shared objects generated for interprocess communication
     delete slowData;
     delete rateData;
     delete fileData;
 
 
-    std::cout << "Closing ORCHID, have a nice day! :-)\n\n" << std::endl;
+    BOOST_LOG_SEV(lg, Information)  << "ORCHID has successfully shut down, have a nice day! :-)\n\n" << std::flush;
 
     return 0;
 }
