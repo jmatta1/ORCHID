b17e810bd160a7a9e7223134c6fb4bf986d816f6
James Till Matta <jamesmatta@gmail.com>
2016-06-06 10:03:54 -0400

    Better code formatting
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Utility/OrchidLogger.h
+++ src/Utility/OrchidLogger.h
@@ -31,12 +31,12 @@
 
 enum LogSeverity
 {
-    Trace=0,
-    Debug=1,
-    Information=2,
-    Warning=3,
-    Error=4,
-    Critical=5
+    Trace       = 0,
+    Debug       = 1,
+    Information = 2,
+    Warning     = 3,
+    Error       = 4,
+    Critical    = 5
 };
 
 //make a global logger
