369cf5b59f17bae7533c933f1948d616108aa24e
James Till Matta <jamesmatta@gmail.com>
2016-06-07 13:57:37 -0400

    Implemented event writer interface base class
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/Events/EventInterface.h
@@ -0,0 +1,48 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file EventInterface.h
+** @author James Till Matta
+** @date 07 June, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the defintion of the reentrant base class for events that are
+** sent to the file output thread
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_ASYNCIO_ASYNCOUTFILE_H
+#define ORCHID_SRC_ASYNCIO_ASYNCOUTFILE_H
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+
+namespace Events
+{
+
+class EventInterface
+{
+public:
+    virtual ~EventInterface(){}
+    
+    //functions to retrieve the binary representation
+    virtual int getSizeOfBinaryRepresentation() = 0;
+    //the buffer provided to this function will be at *least* the size
+    //returned by getSizeOfBinaryRepresentation
+    virtual void getBinaryRepresentation(char* buff) = 0;
+    
+    //functions to retrieve the ascii representation
+    virtual int getSizeOfAsciiRepresentation() = 0;
+    //the buffer provided to this function will be at *least* the size returned
+    //by getSizeOfAsciiRepresentation
+    virtual void getAsciiRepresentation() = 0;
+};
+
+}
