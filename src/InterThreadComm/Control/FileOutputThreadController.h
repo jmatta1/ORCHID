bb29a04e9547bcc1405368ac8499890332866431
James Till Matta <jamesmatta@gmail.com>
2016-06-08 12:11:46 -0400

    Started implementing file output thread controller
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/InterThreadComm/Control/FileOutputThreadController.h
@@ -0,0 +1,42 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file FileOutputThreadController.h
+** @author James Till Matta
+** @date 08 June, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition of the file output thread controller class
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_INTERTHREADCOMM_CONTROL_FILEOUTPUTTHREADCONTROLLER_H
+#define ORCHID_SRC_INTERTHREADCOMM_CONTROL_FILEOUTPUTTHREADCONTROLLER_H
+
+// includes for C system headers
+// includes for C++ system headers
+#include<atomic>
+#include<boost/thread.hpp>
+// includes from other libraries
+// includes from ORCHID
+
+namespace InterThread
+{
+
+class FileOutputThreadController
+{
+public:
+    
+private:
+    
+};
+
+}
+
+#endif //ORCHID_SRC_INTERTHREADCOMM_CONTROL_FILEOUTPUTTHREADCONTROLLER_H
