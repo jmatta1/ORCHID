202d1998ba90b271a0179f05ff365bff4ea1ac5f
James Till Matta <jamesmatta@gmail.com>
2016-06-09 17:17:25 -0400

    Fleshing out the File Output Thread
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Threads/FileOutputThread.cpp
+++ src/Threads/FileOutputThread.cpp
@@ -22,5 +22,105 @@
 #include"FileOutputThread.h"
 // includes for C system headers
 // includes for C++ system headers
+#include<sstream>
+#include<iomanip>
 // includes from other libraries
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+#include<boost/filesystem.hpp>
 // includes from ORCHID
+#include"Utility/OrchidLogger.h"
+
+namespace Threads
+{
+
+FileOutputThread::FileOutputThread(SlowControlEventQueue* incomingSlowControlEvents,
+                                   SlowControlEventQueue* returningSlowControlEvents,
+         //                        ProcessedEventQueue* incomingDigitizerEvents,
+         //                        ProcessedEventQueue* returningDigitizerEvents,
+                                   InterThread::FileData* fileDat,
+                                   InterThread::FileOutputThreadController* fileThreadCtrl,
+                                   InputParser::GeneralBlock* generalBlock):
+    outFile(nullptr), outDirectory(generalBlock->baseOutputDirectory), currentFileName(""),
+    currentRunTitle(generalBlock->runTitle), runNumber(0), sequenceNumber(0),
+    incomingSlowControlEventQueue(incomingSlowControlEvents),
+    returningSlowControlEventQueue(returningSlowControlEvents), fileData(fileDat),
+    fileThreadController(fileThreadCtrl)
+{
+    //make directories and prep file name
+    this->prepNewRunFolder();
+    //load the queue with buffers of the appropriate size
+    for(int i=0; i<BufferCount ; ++i)
+    {
+        char* allocBuffer = new (std::nothrow) char[BufferSizeMB*1024*1024];
+        if(allocBuffer == nullptr)
+        {
+            BOOST_LOG_SEV(OrchidLog::get(), Critical) << "Error In File Write Buffer Allocation";
+            throw std::runtime_error("Error In File Write Buffer Allocation");
+        }
+        //technically this thread should not push, only pop, but I think that it
+        //is legal in this case since concurrent use of the queue has no hope of 
+        //start until after this loop
+        this->bufferQueue.push(allocBuffer);
+    }
+    //make the asynchronous output file
+    this->outFile = new AsyncIO::AsyncOutFile(this->currentFileName, &(this->bufferQueue));
+    
+}
+
+FileOutputThread::~FileOutputThread()
+{
+    delete this->outFile;
+    for(int i=0; i<BufferCount ; ++i)
+    {
+        char* allocBuffer;
+        this->bufferQueue.pop(allocBuffer);
+        delete[] allocBuffer;
+    }
+}
+
+void FileOutputThread::operator()()
+{
+    
+}
+
+void FileOutputThread::prepNewRunFolder()
+{
+    boost::filesystem::path writePath(this->outDirectory);
+    boost::system::error_code mkDirErr;
+    bool success = boost::filesystem::create_directories(writePath, mkDirErr);
+    if(!success)
+    {
+        BOOST_LOG_SEV(OrchidLog::get(), Critical) << "Could not create data directory in file thread";
+        throw std::runtime_error("Could not create data directory in file thread");
+    }
+    writePath.append(this->currentRunTitle);
+    success = boost::filesystem::create_directories(writePath, mkDirErr);
+    if(!success)
+    {
+        BOOST_LOG_SEV(OrchidLog::get(), Critical) << "Could not create run directory in file thread";
+        throw std::runtime_error("Could not create run directory in file thread");
+    }
+    if(!boost::filesystem::is_directory(writePath, mkDirErr))
+    {
+        BOOST_LOG_SEV(OrchidLog::get(), Critical) << "No run directory detected despite creation";
+        throw std::runtime_error("No run directory detected despite creation");
+    }
+    this->writeDirectory = writePath.native();
+    this->buildFileName();
+}
+
+void FileOutputThread::buildFileName()
+{
+    std::ostringstream builder;
+    builder << this->writeDirectory;
+    if(this->writeDirectory.back()!=boost::filesystem::path::preferred_separator)
+    {
+        builder << boost::filesystem::path::preferred_separator;
+    }
+    builder << this->currentRunTitle << "_";
+    builder << std::setw(4) << std::setfill('0') << this->runNumber << ".dat.";
+    builder << std::setw(4) << std::setfill('0') << this->sequenceNumber ;
+    this->currentFileName = builder.str();
+}
+
+}
--- src/Threads/FileOutputThread.h
+++ src/Threads/FileOutputThread.h
@@ -23,6 +23,7 @@
 
 // includes for C system headers
 // includes for C++ system headers
+#include<string>
 // includes from other libraries
 #include<boost/lockfree/queue.hpp>
 #include<boost/lockfree/spsc_queue.hpp>
@@ -30,25 +31,77 @@
 // includes from ORCHID
 #include"InterThreadComm/Control/FileOutputThreadController.h"
 #include"AsyncIO/AsyncOutFile.h"
+#include"InterThreadComm/InterThreadQueueSizes.h"
+#include"Events/SlowControlEvent.h"
+#include"InterThreadComm/Data/FileData.h"
+#include"Events/TriggerEvent.h"
+#include"InputLib/Blocks/GeneralBlock.h"
+
 namespace Threads
 {
-enum {BufferQueueSize=64};
+//256MB of memory for buffers, approx 2 second of HDD write
+enum {BufferCount=128, BufferSizeMB=2};
+//typedef boost::lockfree::spsc_queue<TriggerEvent*, boost::lockfree::capacity<InterThread::getEnumVal(InterThread::QueueSizes::SlowControlToFile)> > ProcessedEventQueue;
+typedef boost::lockfree::spsc_queue<SlowControlEvent*, boost::lockfree::capacity<InterThread::getEnumVal(InterThread::QueueSizes::SlowControlToFile)> > SlowControlEventQueue;
+typedef boost::lockfree::spsc_queue<char*, boost::lockfree::capacity<BufferCount> > BufferQueue;
 
 class FileOutputThread
 {
 public:
-    FileOutputThread();
+    FileOutputThread(SlowControlEventQueue* incomingSlowControlEvents, SlowControlEventQueue* returningSlowControlEvents,
+             //      ProcessedEventQueue* incomingDigitizerEvents, ProcessedEventQueue* returningDigitizerEvents,
+                     InterThread::FileData* fileDat, InterThread::FileOutputThreadController* fileThreadCtrl,
+                     InputParser::GeneralBlock* generalBlock);
     ~FileOutputThread();
     
     void operator()();
 
 private:
+    //make certain that a directory exists, or create it if it doesnt and the like
+    void prepNewRunFolder();
+    //make the file name based on run info
+    void buildFileName();
+    
+    /*
+     * File Data
+     */
     //this is the underlying asynchronous file that we use to dump full buffers
-    AsyncIO::AsyncOutFile outFile;
+    AsyncIO::AsyncOutFile* outFile;
     //this is the underlying queue full of buffers to store data to be sent to
     //the file when the time comes
-    boost::lockfree::spsc_queue<char*, boost::lockfree::capacity<BufferQueueSize> > bufferQueue;
+    BufferQueue bufferQueue;
+    //the directory for all data (sub directories hold different runs)
+    std::string outDirectory;
+    //the directory that we are currently writing to (outDirectory/runTitle/filename.dat.#)
+    std::string writeDirectory;
+    //the file name
+    std::string currentFileName;
+    //the run title
+    std::string currentRunTitle;
+    //the run number
+    int runNumber;
+    //the sequence number for the run
+    int sequenceNumber;
     
+    /*
+     * Interthread Queues
+     */
+    //this is the queue that takes events from the event processing threads to this thread
+    //ProcessedEventQueue* incomingDigitizerEventQueue;
+    //this is the queue that takes empty events from this thread to the event processing threads
+    //ProcessedEventQueue* returningDigitizerEventQueue;
+    //this is the queue that takes events from the slow controls thread to this thread
+    SlowControlEventQueue* incomingSlowControlsEventQueue;
+    //this is the queue that takes empty events from this thread to the slow controls thread
+    SlowControlEventQueue* returningSlowControlsEventQueue;
+    
+    /*
+     * Interthread Statistics and Control
+     */
+    //this is the variable that stores file statistics
+    InterThread::FileData* fileData;
+    //this is the variable that points to the control structure for this thread
+    InterThread::FileOutputThreadController* fileThreadController;
 };
 }
 #endif //ORCHID_SRC_THREADS_FILEOUTPUTTHREAD_H
