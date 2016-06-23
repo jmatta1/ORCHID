271908613b5a507b5fc045a4db374eb250205242
James Till Matta <jamesmatta@gmail.com>
2016-06-23 17:33:49 -0400

    Tweaked implementation, no specialization needed
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/InterThreadComm/QueuePair.h
+++ src/InterThreadComm/QueuePair.h
@@ -28,32 +28,28 @@
 #include<boost/lockfree/queue.hpp>
 #include<boost/lockfree/spsc_queue.hpp>
 // includes from ORCHID
-#include"InterThreadQueueSizes.h"
+#include"QueueTMP.h"
+
 namespace InterThread
 {
 
-template <typename QueueType, typename QueueContents, QueueSizes qSize>
+template <typename QueueContents, typename QueueType>
 class QueuePair
 {
 public:
-    QueuePair():producerWaiting(false), consumerWaiting(false), notForceWake(true),
-        consumerQueue(nullptr), producerQueue(nullptr)
-    {
-        consumerQueue = new QueueType<QueueContents*, boost::lockfree::capacity<getEnumVal(qSize)>>();
-        //by adding this tiny bit of extra size we never need to wait on the return queue, it always works
-        producerQueue = new QueueType<QueueContents*, boost::lockfree::capacity<getEnumVal(qSize)>>();
-    }
+    QueuePair():producerWaiting(false), consumerWaiting(false), notForceWake(true)
+    {}
+    ~QueuePair(){}
     
-    bool pushToConsumerQueue(QueueContents* data);
-    bool popFromConsumerQueue(QueueContents*& data);
-    bool pushToProducerQueue(QueueContents* data);
-    bool popFromProducerQueue(QueueContents*& data);
+    inline bool producerPush(QueueContents* data);
+    bool consumerPop(QueueContents*& data);
+    inline bool consumerPush(QueueContents* data);
+    bool producerPop(QueueContents*& data);
     
-    //these are short and easy functions but I cannot write them here because I am specializing them
-    inline bool tryPushToConsumerQueue(QueueContents* data);
-    inline bool tryPopFromConsumerQueue(QueueContents*& data);
-    inline bool tryPushToProducerQueue(QueueContents* data);
-    inline bool tryPopFromProducerQueue(QueueContents*& data);
+    inline bool tryProducerPush(QueueContents* data);
+    inline bool tryConsumerPop(QueueContents*& data);
+    inline bool tryConsumerPush(QueueContents* data);
+    inline bool tryProducerPop(QueueContents*& data);
     
     void wakeOneConsumer(){if(this->consumerWaiting.load() > 0) this->consumerWaitCondition.notify_one();}
     void wakeOneProducer(){if(this->producerWaiting.load() > 0) this->producerWaitCondition.notify_one();}
@@ -80,19 +76,19 @@ private:
     
     //queues
     //queue for full data objects to be sent from producer to consumer
-    QueueType<QueueContents*, boost::lockfree::capacity<getEnumVal(qSize)>>* consumerQueue;
+    QueueType consumerQueue;
     //queue for empty/read data objects to be sent from consumer to producer
-    QueueType<QueueContents*, boost::lockfree::capacity<getEnumVal(qSize)>>* producerQueue;
+    QueueType producerQueue;
 };
 
 
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::pushToConsumerQueue(QueueContents* data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::producerPush(QueueContents* data)
 {
     //this function pushes a filled event to the consumer queue. This should *always* succeed.
     //The guarantee of success comes because if we managed to get an empty event to fill
     //from the producer queue then there *must* be a slot open in the consumer queue to send it to
-    bool success = this->consumerQueue->push(data);
+    bool success = PushSelect<QueueType, QueueContents>::push(this->consumerQueue, data);
     //since we have enqueued a data object, make certain that the consumer is awake to get it
     if(this->consumerWaiting.load() > 0)//no need to check for success it is guaranteed
     {
@@ -101,12 +97,12 @@ bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::pushToConsume
     return success;
 }
 
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::popFromConsumerQueue(QueueContents*& data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::consumerPop(QueueContents*& data)
 {
     //this function trys to pop from the queue and waits until there is a data available to do so
     bool success = false;
-    while (!(success = this->consumerQueue->pop(data)) && this->notForceWake.load())
+    while (!(success = this->consumerQueue.pop(data)) && this->notForceWake.load())
     {
         //we were not able to pop from the queue, so clearly the producer is lagging
         //make certain the producer is not waiting on something and then wait until the consumer has cleared an element
@@ -124,13 +120,13 @@ bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::popFromConsum
     return success;
 }
 
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::pushToProducerQueue(QueueContents* data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::consumerPush(QueueContents* data)
 {
     //this function tries to push to the producer queue (which returns empty objects to the producer)
     //as, by design, the producer queue has 5 extra slots relative to the consumer queue, and the producer queue
     //was only initially filled by empties to the capacity of the consumer queue, this will *always* succeed.
-    bool success = this->producerQueue->push(data);
+    bool success = PushSelect<QueueType, QueueContents>::push(this->producerQueue, data);
     //if we have enqueued an empty event, make certain that the producer queue knows
     if(this->producerWaiting.load() > 0)//no need to check for success it is guaranteed
     {
@@ -139,13 +135,13 @@ bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::pushToProduce
     return success;
 }
 
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::popFromProducerQueue(QueueContents*& data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::producerPop(QueueContents*& data)
 {
     //this function tries to pop from the producer queue, if it is empty (ie all the buffers are currently in
     //the producer queue) then we wait for the consumer to make some more empties for us to use
     bool success = false;
-    while (!(success = this->producerQueue->pop(data)) && this->notForceWake.load())
+    while (!(success = this->producerQueue.pop(data)) && this->notForceWake.load())
     {
         //if we are here then we are not able to pop from the queue so clearly the
         //consumer is lagging, make certain that the consumer is not waiting for
@@ -164,89 +160,11 @@ bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::popFromProduc
     return success;
 }
 
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::pushToConsumerQueue(QueueContents* data)
-{
-    //this function pushes a filled event to the consumer queue. This should *always* succeed.
-    //The guarantee of success comes because if we managed to get an empty event to fill
-    //from the producer queue then there *must* be a slot open in the consumer queue to send it to
-    bool success =  this->consumerQueue->bounded_push(data);
-    //since we have enqueued a data object, make certain that the consumer is awake to get it
-    if(this->consumerWaiting.load() > 0)//no need to check for success it is guaranteed
-    {
-        this->consumerWaitCondition.notify_one();
-    }
-    return success;
-}
-
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::popFromConsumerQueue(QueueContents*& data)
-{
-    //this function trys to pop from the queue and waits until there is a data available to do so
-    bool success = false;
-    while (!(success = this->consumerQueue->pop(data)) && this->notForceWake.load())
-    {
-        //we were not able to pop from the queue, so clearly the producer is lagging
-        //make certain the producer is not waiting on something and then wait until the consumer has cleared an element
-        if(this->producerWaiting.load() > 0)
-        {
-            this->producerWaitCondition.notify_all();
-        }
-        //now wait for some full events
-        ++(this->consumerWaiting);
-        boost::unique_lock<boost::mutex> waitLock(this->consumerWaitMutex);
-        this->consumerWaitCondition.wait(waitLock);
-        //if we are here then we have woken from the wait, note this
-        --(this->consumerWaiting);
-    }
-    return success;
-}
-
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::pushToProducerQueue(QueueContents* data)
-{
-    //this function tries to push to the producer queue (which returns empty objects to the producer)
-    //as, by design, the producer queue has 5 extra slots relative to the consumer queue, and the producer queue
-    //was only initially filled by empties to the capacity of the consumer queue, this will *always* succeed.
-    bool success = this->producerQueue->bounded_push(data);
-    //if we have enqueued an empty event, make certain that the producer queue knows
-    if(this->producerWaiting.load() > 0)//no need to check for success it is guaranteed
-    {
-        this->producerWaitCondition.notify_one();
-    }
-    return success;
-}
-
-template <typename QueueContents, QueueSizes qSize>
-bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::popFromProducerQueue(QueueContents*& data)
-{
-    //this function tries to pop from the producer queue, if it is empty (ie all the buffers are currently in
-    //the producer queue) then we wait for the consumer to make some more empties for us to use
-    bool success = false;
-    while (!(success = this->producerQueue->pop(data)) && this->notForceWake.load())
-    {
-        //if we are here then we are not able to pop from the queue so clearly the
-        //consumer is lagging, make certain that the consumer is not waiting for
-        //some silly reason
-        if(this->consumerWaiting.load())
-        {
-            this->consumerWaitCondition->notify_all();
-        }
-        //now wait for some empty events
-        ++(this->producerWaiting);
-        boost::unique_lock<boost::mutex> waitLock(this->producerWaitMutex);
-        this->producerWaitCondition.wait(waitLock);
-        //if we are here then we need to note that we have woken up
-        --(this->producerWaiting);
-    }
-    return success;
-}
-
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::tryPushToConsumerQueue(QueueContents* data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::tryProducerPush(QueueContents* data)
 {
     //as stated above, push always works, therefor push, wake and return
-    bool success = this->consumerQueue->push(data);
+    bool success = PushSelect<QueueType, QueueContents>::push(this->consumerQueue, data);
     //since we have enqueued a data object, make certain that the consumer is awake to get it
     if(this->consumerWaiting.load() > 0)//no need to check for success it is guaranteed
     {
@@ -255,17 +173,17 @@ inline bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::tryPus
     return success;
 }
 
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::tryPopFromConsumerQueue(QueueContents*& data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::tryConsumerPop(QueueContents*& data)
 {
     //try to pop from the consumer queue, return true for success or false for failure
-    return this->consumerQueue->pop(data);
+    return this->consumerQueue.pop(data);
 }
 
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::tryPushToProducerQueue(QueueContents* data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::tryConsumerPush(QueueContents* data)
 {
-    bool success =  this->producerQueue->push(data);
+    bool success = PushSelect<QueueType, QueueContents>::push(this->producerQueue, data);
     //since we have enqueued a data object, make certain that the consumer is awake to get it
     if(this->producerWaiting.load() > 0)//no need to check for success it is guaranteed
     {
@@ -274,51 +192,13 @@ inline bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::tryPus
     return success;
 }
 
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::spsc_queue, QueueContents, qSize>::tryPopFromProducerQueue(QueueContents*& data)
+template <typename QueueContents, typename QueueType>
+bool QueuePair<QueueContents, QueueType>::tryProducerPop(QueueContents*& data)
 {
     //try to pop from the producer queue
-    return this->producerQueue->pop(data);
+    return this->producerQueue.pop(data);
 }
 
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::tryPushToConsumerQueue(QueueContents* data)
-{
-    //as stated above, push always works, therefor push, wake and return
-    bool success = this->consumerQueue->bounded_push(data);
-    //since we have enqueued a data object, make certain that the consumer is awake to get it
-    if(this->consumerWaiting.load() > 0)//no need to check for success it is guaranteed
-    {
-        this->consumerWaitCondition.notify_one();
-    }
-    return success;
-}
-
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::tryPopFromConsumerQueue(QueueContents*& data)
-{
-    //try to pop from the consumer queue, return true for success or false for failure
-    return this->consumerQueue->pop(data);
-}
-
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::tryPushToProducerQueue(QueueContents* data)
-{
-    bool success =  this->producerQueue->bounded_push(data);
-    //since we have enqueued a data object, make certain that the consumer is awake to get it
-    if(this->producerWaiting.load() > 0)//no need to check for success it is guaranteed
-    {
-        this->producerWaitCondition.notify_one();
-    }
-    return success;
-}
-
-template <typename QueueContents, QueueSizes qSize>
-inline bool QueuePair<boost::lockfree::queue, QueueContents, qSize>::tryPopFromProducerQueue(QueueContents*& data)
-{
-    //try to pop from the producer queue
-    return this->producerQueue->pop(data);
-}
 
 }
 #endif //ORCHID_SRC_INTERTHREADCOMM_QUEUEPAIR_H
