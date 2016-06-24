4603ae3fff1beaef79235344db4ccd9c8fe494f3
James Till Matta <jamesmatta@gmail.com>
2016-06-24 17:34:23 -0400

    Added file to hold long queue template types
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/Utility/CommonTypeDefs.h
@@ -0,0 +1,41 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file FileOutputThread.h
+** @author James Till Matta
+** @date 24 June, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds a few type defs of heavily templated types for my sanity
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_UTILITY_COMMONTYPEDEFS_H
+#define ORCHID_SRC_UTILITY_COMMONTYPEDEFS_H
+
+#include"Events/EventInterface.h"
+#include"InterThreadComm/InterThreadQueueSizes.h"
+#include"InterThreadComm/MultiQueuePair.h"
+#include"InterThreadComm/QueuePair.h"
+
+namespace Utility
+{
+//queue types
+typedef boost::lockfree::queue<Events::EventInterface*, boost::lockfree::capacity<InterThread::getEnumVal(InterThread::QueueSizes::ProcessingToFile)> > ProcessedEventQueue;
+typedef boost::lockfree::spsc_queue<Events::EventInterface*, boost::lockfree::capacity<InterThread::getEnumVal(InterThread::QueueSizes::SlowControlToFile)> > SlowControlEventQueue;
+//typedef boost::lockfree::queue<char*, boost::lockfree::capacity<InterThread::getEnumVal(InterThread::QueueSizes::DigitizerToProcessing)> > ToProcessingQueue;
+
+//Queues for sending things to file type
+typedef InterThread::MultiQueuePair<Events::EventInterface*, ProcessedEventQueue, SlowControlEventQueue> ToFileMultiQueue;
+enum{ProcessingQueueIndex = 0, SlowControlsQueueIndex = 1};
+
+//queues for sending things from digitizer to processing
+//typedef InterThread::QueuePair<char*, ToProcessingQueue> ToProcessingQueuePair;
+}
+#endif //ORCHID_SRC_UTILITY_COMMONTYPEDEFS_H
