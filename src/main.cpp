f707e90ac27c9f21b7b0312d7746507351694fe5
James Till Matta <jamesmatta@gmail.com>
2016-08-08 14:17:08 -0400

    Added reading of digitizer module file
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -86,11 +86,14 @@ int main(int argc, char* argv[])
     InputParser::InputParameters params;
     InputParser::MpodChannelData mpodChannelData;
     InputParser::MpodModuleData mpodModuleData;
+    InputParser::DigitizerModuleData digitizerModuleData;
+    
     //TODO: Add reading of digitizer module and channel csv files
     //TODO: Also add the reading of the mapping csv which maps digitizer channel to voltage channel
     if(!Utility::parseAndValidateInput(params, inputFileName) ||
        !Utility::parseAndValidateMpodModule(mpodModuleData, params.powerBlock->perModuleParameterFile) ||
-       !Utility::parseAndValidateMpodChannel(mpodChannelData, params.powerBlock->perChannelParameterFile) )
+       !Utility::parseAndValidateMpodChannel(mpodChannelData, params.powerBlock->perChannelParameterFile) ||
+       !Utility::parseAndValidateDigitizerModule(digitizerModuleData, params.digitizerBlock->perModuleParameterFile))
     {
         return 1;
     }
@@ -110,6 +113,10 @@ int main(int argc, char* argv[])
     BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
     BOOST_LOG_SEV(lg, Information)  << "MPOD Channel Input File";
     BOOST_LOG_SEV(lg, Information)  << mpodChannelData ;
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
+    BOOST_LOG_SEV(lg, Information)  << "Digitizer Module Input File";
+    BOOST_LOG_SEV(lg, Information)  << digitizerModuleData ;
     BOOST_LOG_SEV(lg, Information)  << "--------------------------------------------------------------------------------";
     //TODO: output the data from the digitizer csvs and the mapping csv
     BOOST_LOG_SEV(lg, Trace)  << "\nReady to start!" << std::flush;
