c122f1d0c2bea111e4d8ef69fca883dc82eaa75b
James Till Matta <jamesmatta@gmail.com>
2016-08-17 11:15:21 -0400

    Implemented the DPP-PSD event class
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/Events/DppPsdEvent.cpp
@@ -0,0 +1,65 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file DppPsdEvent.cpp
+** @author James Till Matta
+** @date 21 June, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds definition of the dpp psd event extension of an event interface
+**
+********************************************************************************
+*******************************************************************************/
+#include"DppPsdEvent.h"
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+
+namespace Events
+{
+
+
+int DppPsdEvent::getSizeOfBinaryRepresentation()
+{
+    return (sizeof(unsigned long long) + 7*sizeof(unsigned short));
+}
+
+unsigned short boardNumber;
+unsigned short channelNumber;
+unsigned long long extendedTimestamp;
+unsigned short longIntegral;
+unsigned short shortIntegral;
+//could be zero based on extras word options
+unsigned short fineTimeStamp;
+unsigned short baseline;
+unsigned short flags;//bit[0] = PUR flag, bit[1] = over-range, bit[2] = trigger lost
+
+void DppPsdEvent::getBinaryRepresentation(char* buff)
+{
+    int index = 0;
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = boardNumber;
+    index += sizeof(unsigned short);
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = channelNumber;
+    index += sizeof(unsigned short);
+    *(reinterpret_cast<unsigned long long*>(&(buff[index]))) = extendedTimestamp;
+    index += sizeof(unsigned long long);
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = longIntegral;
+    index += sizeof(unsigned short);
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = shortIntegral;
+    index += sizeof(unsigned short);
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = fineTimeStamp;
+    index += sizeof(unsigned short);
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = baseline;
+    index += sizeof(unsigned short);
+    *(reinterpret_cast<unsigned short*>(&(buff[index]))) = flags;
+    index += sizeof(unsigned short);
+}
+
+}
---
+++ src/Events/DppPsdEvent.h
@@ -0,0 +1,68 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file DppPsdEvent.h
+** @author James Till Matta
+** @date 21 June, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds definition of the dpp psd event extension of an event interface
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_EVENTS_DPPPSDEVENT_H
+#define ORCHID_SRC_EVENTS_DPPPSDEVENT_H
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+#include"EventInterface.h"
+
+namespace Events
+{
+
+class DppPsdEvent : public EventInterface
+{
+public:
+    DppPsdEvent():boardNumber(0), channelNumber(0), longIntegral(0),
+        shortIntegral(0), extendedTimestamp(0), fineTimeStamp(0),
+        baseline(0), flags(0) {}
+    virtual ~DppPsdEvent(){}
+    
+    //functions to retrieve the binary representation
+    virtual int getSizeOfBinaryRepresentation();
+    //the buffer provided to this function will be at *least* the size
+    //returned by getSizeOfBinaryRepresentation
+    virtual void getBinaryRepresentation(char* buff);
+    
+    //setters for the event processing threads to dump data into these
+    void setBoard(unsigned short board){boardNumber = board;}
+    void setChannel(unsigned short chan){channelNumber = chan;}
+    void setLongGate(unsigned short lGate){longIntegral = lGate;} 
+    void setShortGate(unsigned short sGate){shortIntegral = sGate;}
+    void setTimeStamp(unsigned long long tStamp){extendedTimestamp = tStamp;}
+    void setFineTimeStamp(unsigned short ftStamp){fineTimeStamp = ftStamp;}
+    void setBaseline(unsigned short bsl){baseline = bsl;}
+    void setFlags(unsigned short flg){flags = flg;}
+    
+private:
+    unsigned short boardNumber;
+    unsigned short channelNumber;
+    unsigned long long extendedTimestamp;
+    unsigned short longIntegral;
+    unsigned short shortIntegral;
+    unsigned short fineTimeStamp;
+    unsigned short baseline;//could be zero based on extras word options
+    unsigned short flags;//bit[0] = PUR flag, bit[1] = over-range, bit[2] = trigger lost
+};
+
+
+}
+
+#endif  ORCHID_SRC_EVENTS_DPPPSDEVENT_H
