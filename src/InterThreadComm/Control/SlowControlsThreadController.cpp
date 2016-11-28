4c8cc596eaf210fd6cb21cad5c9696fe88882524
James Till Matta <jamesmatta@gmail.com>
2016-11-28 09:22:57 -0500

    Fixed a weird data race and added a sleep
    The data race, which in normal usage should be super uncommon, could have
    screwed things up if a really odd usage condition occurred. Here I have
    fixed it. The second thing that I have done is to add a sleep function.
    This allows the slow controls thread to sleep using the wait condition,
    allowing it to be responsive to state changes, now instead of waiting for it
    to wake up from it using this_thread::sleep(duration) the change of state
    hits the wait condition that it is using to sleep in the first place causing
    it to wake up immediately.
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/InterThreadComm/Control/SlowControlsThreadController.cpp
+++ src/InterThreadComm/Control/SlowControlsThreadController.cpp
@@ -43,5 +43,30 @@ void SlowControlsThreadController::waitForNewState()
     }
 }
 
+void SlowControlsThreadController::slowControlsThreadSleep(const boost::chrono::nanoseconds& sleepTime)
+{
+    //figure out the time that we need to sleep until
+    boost::chrono::time_point endTime(boost::chrono::system_clock::now() + sleepTime);
+    //set up the variable that will hold how the condition variable returned (timeout or something else)
+    boost::cv_status condVarStatus(boost::cv_status::no_timeout);
+    //lock the wait mutex
+    boost::unique_lock<boost::mutex> waitLock(this->waitMutex);
+    //store the current state to check against
+    SlowControlsThreadState oldState = this->currentState.load();
+    //use the condition variable to sleep. We do this instead of this_thread::sleep because this way
+    //another thread can wake us prematurely if there was a change of state while we are sleeping
+    //allowing a faster response to startup and shutdown of acquisition. To handle this we have the
+    //loop (which squashes spurious wakeups (however infrequent they are) by check two conditions, the first
+    //condition is if the wait returned timeout, if it did then we woke up naturally and it is time to resume
+    //if we did not get time out, but instead the old state is modified, then we have a condition where we are
+    //being woke up early because of a condition change so we can respond to it. If neither condition is true
+    //then we had a spurious wakeup and we need to go back to sleep until our chosen time
+    while(oldState == this->currentState.load() && condVarStatus != boost::cv_status::timeout)
+    {
+        this->threadSleeping.store(true);
+        condVarStatus = this->waitCondition.wait_until(waitLock, endTime);
+        this->threadSleeping.store(false);
+    }
+}
 
 }
--- src/InterThreadComm/Control/SlowControlsThreadController.h
+++ src/InterThreadComm/Control/SlowControlsThreadController.h
@@ -22,6 +22,7 @@
 // includes for C system headers
 // includes for C++ system headers
 #include<atomic>
+#include<boost/chrono.hpp>
 #include<boost/thread.hpp>
 // includes from other libraries
 // includes from ORCHID
@@ -37,36 +38,82 @@ public:
     //construction and destruction
     SlowControlsThreadController():
         currentState(SlowControlsThreadState::Stopped), threadWaiting(false),
-        threadDone(false){}
+        threadSleeping(false), threadDone(false){}
     ~SlowControlsThreadController(){}
     
     //functions for the slow controls thread to access
     SlowControlsThreadState getState(){return this->currentState.load();}
     void waitForNewState();
+    void slowControlsThreadSleep(const boost::chrono::nanoseconds& sleepTime);
     void setDone(){this->threadDone.store(true);}
     
     //functions for the UI thread to access
     void setToTerminate()
     {
-        this->currentState.store(SlowControlsThreadState::Terminate);
+        //enter an artificial block to create and destroy lock before we notify
+        //the lock prevents a weird data race that, in a very strange and possibly
+        //incorrect usage mode, cause the slowControls thread to wait without
+        //end. This would happen because it would start waiting for a change
+        //(or to sleep) and after it locks the mutex, but before it grabs the
+        //current state the state is changed. That would then cause it to think
+        //that the changed state was the old state and it would wait for yet
+        //another state change, this way it cannot have that happen
+        {
+            boost::unique_lock<boost::mutex> lock(this->waitMutex);
+            this->currentState.store(SlowControlsThreadState::Terminate);
+        }
         this->waitCondition.notify_all();
     }
     void setToStop()
     {
-        this->currentState.store(SlowControlsThreadState::Stopped);
+        //enter an artificial block to create and destroy lock before we notify
+        //the lock prevents a weird data race that, in a very strange and possibly
+        //incorrect usage mode, cause the slowControls thread to wait without
+        //end. This would happen because it would start waiting for a change
+        //(or to sleep) and after it locks the mutex, but before it grabs the
+        //current state the state is changed. That would then cause it to think
+        //that the changed state was the old state and it would wait for yet
+        //another state change, this way it cannot have that happen
+        {
+            boost::unique_lock<boost::mutex> lock(this->waitMutex);
+            this->currentState.store(SlowControlsThreadState::Stopped);
+        }
         this->waitCondition.notify_all();
     }
     void setToPolling()
     {
-        this->currentState.store(SlowControlsThreadState::Polling);
+        //enter an artificial block to create and destroy lock before we notify
+        //the lock prevents a weird data race that, in a very strange and possibly
+        //incorrect usage mode, cause the slowControls thread to wait without
+        //end. This would happen because it would start waiting for a change
+        //(or to sleep) and after it locks the mutex, but before it grabs the
+        //current state the state is changed. That would then cause it to think
+        //that the changed state was the old state and it would wait for yet
+        //another state change, this way it cannot have that happen
+        {
+            boost::unique_lock<boost::mutex> lock(this->waitMutex);
+            this->currentState.store(SlowControlsThreadState::Polling);
+        }
         this->waitCondition.notify_all();
     }
     void setToWriting()
     {
-        this->currentState.store(SlowControlsThreadState::Writing);
+        //enter an artificial block to create and destroy lock before we notify
+        //the lock prevents a weird data race that, in a very strange and possibly
+        //incorrect usage mode, cause the slowControls thread to wait without
+        //end. This would happen because it would start waiting for a change
+        //(or to sleep) and after it locks the mutex, but before it grabs the
+        //current state the state is changed. That would then cause it to think
+        //that the changed state was the old state and it would wait for yet
+        //another state change, this way it cannot have that happen
+        {
+            boost::unique_lock<boost::mutex> lock(this->waitMutex);
+            this->currentState.store(SlowControlsThreadState::Writing);
+        }
         this->waitCondition.notify_all();
     }
     bool isWaiting(){return this->threadWaiting.load();}
+    bool isSleeping(){return this->threadSleeping.load();}
     bool isDone(){return this->threadDone.load();}
     
 private:
@@ -76,6 +123,7 @@ private:
     boost::mutex waitMutex;
     boost::condition_variable waitCondition;
     std::atomic_bool threadWaiting;
+    std::atomic_bool threadSleeping;
     
     //termination checking
     std::atomic_bool threadDone;
