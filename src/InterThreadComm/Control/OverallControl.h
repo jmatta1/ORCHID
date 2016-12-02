3ad2cab92b7efe04890c5d8a79e0628bd9e140d8
James Till Matta <jamesmatta@gmail.com>
2016-12-02 17:54:44 -0500

    Started writing OverallControll
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/InterThreadComm/Control/OverallControl.h
+++ src/InterThreadComm/Control/OverallControl.h
@@ -7,12 +7,13 @@
  *****************************************************************************/
 //includes from C libraries
 //includes from C++ libraries
+#include<string>
 //includes from other libraries
 //includes from ORCHID
 #include"SlowControlsThreadController.h"
 #include"AcquisitionThreadControl.h"
-#include"FileOutputThreadController.h"
 #include"ProcessingThreadControl.h"
+#include"InterThreadComm/Data/FileData.h"
 
 namespace InterThread
 {
@@ -20,8 +21,26 @@ namespace InterThread
 class OverallThreadControl
 {
 public:
-    OverallThreadControl(AcquisitionThreadControl* acqCtrl, 
+    OverallThreadControl(AcquisitionThreadControl* acqCtrl, ProcessingThreadControl* prcCtrl,
+                         SlowControlsThreadController* slwCtrl, FileData* filDat);
+    ~OverallThreadControl(){}
+    
+    //sends terminate and waits for response to groups of threads in the following order:
+    //first turn off the production of data
+    //1) acqusition threads
+    //2) slow controls thread
+    //then shutdown the processing and writing of data
+    //3) processing threads
+    void shutDown();
+    //puts everyone into stop condition
+    void idleEverything();
+    void pollSlowControls();
+    void startAcquisition(const std::string& runTitle, int runNum);
 private:
+    AcquisitionThreadControl* acquisitionControl;
+    ProcessingThreadControl* processingControl;
+    SlowControlsThreadController* slowcontrolsControl;
+    FileData* fileData;
 
 };
 }
