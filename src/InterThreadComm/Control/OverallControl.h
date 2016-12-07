50e9a6539d1a0a264b36fa90d86de3be8c57dba1
James Till Matta <jamesmatta@gmail.com>
2016-12-07 17:11:39 -0500

    Started writing Overall control class definition
    Will add more to definition as I rewrite UI thread and see what needs to be
    done
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/InterThreadComm/Control/OverallControl.h
+++ src/InterThreadComm/Control/OverallControl.h
@@ -1,10 +1,22 @@
-/****************************************************************************
- *
- * File OverallControl.h
- *
- * Holds the definition for the OverallControl class which handles coordinating
- *    all the thread controls
- *****************************************************************************/
+/***************************************************************************//**
+********************************************************************************
+**
+** @file OverallControl.h
+** @author James Till Matta
+** @date 1 Dec, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition for the OverallControl class which handles
+**    coordinating all the thread controls
+**
+********************************************************************************
+*******************************************************************************/
 //includes from C libraries
 //includes from C++ libraries
 #include<string>
@@ -21,9 +33,15 @@ namespace InterThread
 class OverallControl
 {
 public:
-    OverallControl(AcquisitionThreadControl* acqCtrl, ProcessingThreadControl* prcCtrl,
-                   SlowControlsThreadController* slwCtrl, FileData* filDat);
+    OverallControl();
     ~OverallControl(){}
+    
+    //retrieves individual controllers for the threads
+    FileData* getFileDataCtrl(){return fileData;}
+    SlowControlsThreadController* getSlowControlsCtrl(){return slowControlsControl;}
+    ProcessingThreadControl* getProcessingThreadCtrl(){return processingControl;}
+    AcquisitionThreadControl* getAcquisitionThreadCtrl(){return acquistionControl;}
+    
     
     //sends terminate and waits for response to groups of threads in the following order:
     //first turn off the production of data
@@ -32,14 +50,20 @@ public:
     //then shutdown the processing and writing of data
     //3) processing threads
     void shutDown();
-    //puts everyone into stop condition
+    //puts everyone into stop condition first acquisition threads, then slow ctrls threads,
+    //then processing threads
     void idleEverything();
+    //first calls idle everything to put it all in stop mode. It then puts the
+    //slow controlls thread in polling mode
     void pollSlowControls();
+    //sets the file data using the given runTitle and runNum, then puts the
+    //slow controls thread into writing mode, puts the processing threads into
+    //active mode, and finally puts the acquisition threads into acquiring mode
     void startAcquisition(const std::string& runTitle, int runNum);
 private:
     AcquisitionThreadControl* acquisitionControl;
     ProcessingThreadControl* processingControl;
-    SlowControlsThreadController* slowcontrolsControl;
+    SlowControlsThreadController* slowControlsControl;
     FileData* fileData;
 
 };
