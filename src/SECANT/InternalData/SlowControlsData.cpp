e49fd7b6997ba8b2aa409ed0cf6e905449a10761
James Till Matta <jamesmatta@gmail.com>
2017-04-06 17:02:09 -0400

    Setting up generic slow control polling info
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/SECANT/InternalData/SlowControlsData.cpp
@@ -0,0 +1,37 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file SlowData.cpp
+** @author James Till Matta
+** @date 27 Apr, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the implementation of the struct that stores the data
+** garnished from the slow controls like voltage and temperature
+**
+********************************************************************************
+*******************************************************************************/
+
+#include"SlowData.h"
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHID
+#include"Hardware/HVLib/VoltageData.h"
+
+namespace Secant
+{
+
+namespace OperationData
+{
+
+
+
+}
+}
---
+++ src/SECANT/InternalData/SlowControlsData.h
@@ -0,0 +1,47 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file SlowData.h
+** @author James Till Matta
+** @date 27 Apr, 2016
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition of the struct that stores the data garnished
+** from the slow controls like voltage and temperature
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHID_SRC_SLOWCONTROLS_SLOWDATA_H
+#define ORCHID_SRC_SLOWCONTROLS_SLOWDATA_H
+
+// includes for C system headers
+// includes for C++ system headers
+#include<atomic>
+#include<string>
+// includes from other libraries
+// includes from ORCHID
+#include"Hardware/HVLib/VoltageData.h"
+
+namespace Secant
+{
+
+namespace OperationData
+{
+
+class SlowControlsData
+{
+public:
+    
+private:
+    
+};
+}
+}
+
+#endif //ORCHID_SRC_SLOWCONTROLS_SLOWDATA_H
