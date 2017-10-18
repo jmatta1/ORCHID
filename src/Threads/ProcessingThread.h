ffcf69700e92898f9d4af81b717c0837e7564d90
James Till Matta <jamesmatta@gmail.com>
2017-10-18 10:22:53 -0400

    Removed unecessary member var
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Threads/ProcessingThread.h
+++ src/Threads/ProcessingThread.h
@@ -64,7 +64,6 @@ private:
     bool notTerminated;
     int threadNumber;
     long long counters[16];
-    bool sawError = false;
     
     //logger
     boost::log::sources::severity_logger_mt<LogSeverity>& lg;
